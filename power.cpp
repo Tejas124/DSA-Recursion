6936b9e864e879515c3e997f978797eb685c8aac
Tejas Sanjay Shinde <79836871+Tejas124@users.noreply.github.com>
2023-02-06 22:16:50 +0530

    Create power.cpp

---
+++ power.cpp
@@ -0,0 +1,33 @@
+#include<iostream>
+using namespace std;
+
+int power(int a, int b){
+
+    //base case
+    if(b == 0)
+        return 1;
+    
+    if(b == 1)
+        return a;
+
+    //RECURSIVE CALL
+    int ans = power(a, b/2);
+  
+    if(b%2 == 0)
+        return ans*ans;
+    
+    else
+        return a*ans*ans;
+}
+
+int main(){
+
+    int a, b;
+    cin >> a >> b;
+
+    cout<<power(a,b);
+
+    
+
+    return 0;
+}
